edf0f13f7807e9b4c62172785bece70d13fda796
Nestor Sanchez <nsanchez@smartbeemo.com>
2016-05-09 14:57:33 -0400

    Fixed favorites & parse date function

--- www/app/filters/parse-date.js
+++ www/app/filters/parse-date.js
@@ -3,7 +3,9 @@ app.filter('parseDate', function ($filter) {
         if(typeof text === "undefined")
             return text;
         else{
-            var  date = new Date(text.replace(/-/g,"/"));
+            var parts = text.match(/(\d+)/g);
+            var date = new Date(parts[0], parts[1]-1, parts[2]);
+            //var  date = new Date(text.replace(/-/g,"/"));
             return $filter('date')(date, type);
         }
     }
