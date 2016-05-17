02fbbc4727a87ed5c625000c673defa6350b070b
mauro <mauro@icox.com>
2016-05-17 16:15:27 -0300

    update ios 17-05-2016
    Signed-off-by: mauro <mauro@icox.com>

--- www/app/services/database.js
+++ www/app/services/database.js
@@ -1,23 +1,27 @@
 'use strict';
 app.factory('DBService', function ($q, DB_CONFIG) {
     var self = this;
-    self.db = window.sqlitePlugin.openDatabase({name: DB_CONFIG.name, location: 1});
+    self.db = null;
 
     self.init = function(drop) {
-        // Use self.db = window.sqlitePlugin.openDatabase({name: DB_CONFIG.name}); in production
-        self.db = window.sqlitePlugin.openDatabase({name: DB_CONFIG.name, location: 1});
+            self.db = window.sqlitePlugin.openDatabase({name: DB_CONFIG.name, location: 1});
+            //self.db = window.openDatabase(DB_CONFIG.name, '1.0', 'database', -1);
 
+            //self.query('DROP TABLE IF EXISTS artists');
+            angular.forEach(DB_CONFIG.tables, function(table) {
+                var columns = [];
+                if(drop && table.erasable)
+                    self.query('DROP TABLE IF EXISTS '+table.name);
 
-        //self.query('DROP TABLE IF EXISTS artists');
-        angular.forEach(DB_CONFIG.tables, function(table) {
-            var columns = [];
-            if(drop && table.erasable)
-                self.query('DROP TABLE IF EXISTS '+table.name);
+                angular.forEach(table.columns, function(column) {
+                    columns.push(column.name + ' ' + column.type);
+                });
 
-            angular.forEach(table.columns, function(column) {
-                columns.push(column.name + ' ' + column.type);
+               var query = 'CREATE TABLE IF NOT EXISTS ' + table.name + ' (' + columns.join(',') + ')';
+               //var query = 'CREATE TABLE ' + table.name + ' (' + columns.join(',') + ')';
+               self.query(query);
+               //console.log('Table ' + table.name + ' initialized');
             });
-        });   
     };
 
     self.query = function(query, bindings) {
