a8a18231d3105167097f5b5e9dbb0aef68e70967
nestor91sanchez <sanchez91nestor@gmail.com>
2016-12-14 11:57:05 -0400

    adding changes 13-12-2016

--- certs/build.sh
+++ certs/build.sh
@@ -7,7 +7,6 @@ then
     echo "BUILDING RELEASE"
     gulp && ionic build android --release
     cd certs
-    rm LesArts.apk
     echo 'iY88bR62' | jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore ../platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
     /opt/android-sdk/build-tools/23.0.1/zipalign -v 4 ../platforms/android/build/outputs/apk/android-release-unsigned.apk CaboDePlata.apk
     cd ..
